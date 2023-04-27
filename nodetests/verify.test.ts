71879286f8b0eec9814e6a667f9c6d95101c4f72
DarVoid <jorgemduque@hotmail.com>
2023-04-27 21:17:54 +0100

    [start] project

---
+++ nodetests/verify.test.ts
@@ -0,0 +1,7 @@
+import {describe,it,expect} from 'node:test'
+describe('testname', ()=> {
+    it('should do something nice', ()=> {
+        // do something
+        expect().toEqual //.toThrow //.to...
+    })
+})
