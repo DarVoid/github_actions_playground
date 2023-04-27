5ca3b97336d3fd0a92cd7a1cd8793509c37c1ac8
DarVoid <jorgemduque@hotmail.com>
2023-04-27 22:37:02 +0100

    [add] read yaml test

--- src/tests/verify.test.ts
+++ src/tests/verify.test.ts
@@ -2,13 +2,15 @@ import assert from 'node:assert';
 import  {describe, it} from 'node:test'
 import { parse, stringify } from 'yaml'
 import fs from 'fs'
+import * as path from 'path'
+
+const file = fs.readFileSync(path.join(__dirname, "../data/verifythis.yaml"), 'utf-8')
 
 describe('check for objects', ()=> {
-    const file = fs.readFileSync('../data/verifythis.yml', 'utf8')
     it('should do something nice', ()=> {
         let a = parse(file)
-        assert.equal(a,2)
-
+        assert.equal(a.enable, true)
+        assert.equal(a.cenas.isArray(), true)
     })
 
 })
